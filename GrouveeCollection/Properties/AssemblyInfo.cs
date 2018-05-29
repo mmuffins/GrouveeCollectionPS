d829e1ac2f4ddf7199d8146be47d5e68681a674b
mmuffins <mail@tiv73.com>
2018-05-29 23:24:03 +0200

    Updated readme.

--- GrouveeCollection/Properties/AssemblyInfo.cs
+++ GrouveeCollection/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("GrouveeCollection")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A parser for grouvee.com collection export files.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Michael Kellner")]
 [assembly: AssemblyProduct("GrouveeCollection")]
 [assembly: AssemblyCopyright("Copyright © 2018")]
 [assembly: AssemblyTrademark("")]
---
+++ README.md
@@ -0,0 +1,6 @@
+# Grouvee Collection Powershell Module
+
+A powershell module that uses the [Grouvee Collection Parser library](https://github.com/mmuffins/) to parse .csv files generated when exporting a collection from grouvee.com.
+
+## Installation
+
